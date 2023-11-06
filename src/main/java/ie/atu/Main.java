b33df231fee18477b2e9ff84b772e3ed2d2f00c4
G00420041@atu.ie <G00420041@atu.ie>
2023-11-06 10:52:32 +0000

    tried adding for loop

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,5 +1,6 @@
 package ie.atu;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -7,43 +8,42 @@ public class Main {
     {
 
 
-        int[] anArray;
-
-        anArray = new int[10];
-
-
-        Scanner scanner = new Scanner(System.in);
-
-        anArray[0] = 100;
-        anArray[1] = 100;
-        anArray[2] = 100;
-        anArray[3] = 100;
-        anArray[4] = 100;
-        anArray[5] = 100;
-        anArray[6] = 100;
-        anArray[7] = 100;
-        anArray[8] = 100;
-        anArray[9] = 100;
+        //  int[] anArray;
+        //  anArray = new int[10];
+        //  anArray[0] = 100;
+        //  anArray[1] = 200;
+        //  anArray[2] = 300;
+        //  anArray[3] = 400;
+        //  anArray[4] = 500;
+        //  anArray[5] = 600;
+        //  anArray[6] = 700;
+        //  anArray[7] = 800;
+        //  anArray[8] = 900;
+        //  anArray[9] = 1000;
 
 
-        System.out.println("Element at index 0: " + anArray[0]);
-        System.out.println("Element at index 1: " + anArray[1]);
-        System.out.println("Element at index 2: " + anArray[2]);
-        System.out.println("Element at index 3: " + anArray[3]);
-        System.out.println("Element at index 4: " + anArray[4]);
-        System.out.println("Element at index 5: " + anArray[5]);
-        System.out.println("Element at index 6: " + anArray[6]);
-        System.out.println("Element at index 7: " + anArray[7]);
-        System.out.println("Element at index 8: " + anArray[8]);
-        System.out.println("Element at index 9: " + anArray[9]);
+        ArrayList<Integer> num = new ArrayList<Integer>();
+
+        num.add(100);
+        num.add(200);
+        num.add(300);
+        num.add(400);
+        num.add(500);
+        num.add(600);
+        num.add(700);
+        num.add(800);
+        num.add(900);
+        num.add(1000);
+
+
+   int Value = 0;
+
+   for (num : Value) {
+     System.out.println(" Element at index: \n" + num);
+     Value++;
     }
 
-
-
-
-
-
-
-
+        System.out.println("Please enter a new Number: ");
+        Scan;
 
 }

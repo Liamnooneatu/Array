deb588f2f16f842b43cfcf84595acbe2f2233be5
G00420041@atu.ie <G00420041@atu.ie>
2023-11-06 09:45:30 +0000

    added file

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,49 @@
+package ie.atu;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+
+        int[] anArray;
+
+        anArray = new int[10];
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        anArray[0] = 100;
+        anArray[1] = 100;
+        anArray[2] = 100;
+        anArray[3] = 100;
+        anArray[4] = 100;
+        anArray[5] = 100;
+        anArray[6] = 100;
+        anArray[7] = 100;
+        anArray[8] = 100;
+        anArray[9] = 100;
+
+
+        System.out.println("Element at index 0: " + anArray[0]);
+        System.out.println("Element at index 1: " + anArray[1]);
+        System.out.println("Element at index 2: " + anArray[2]);
+        System.out.println("Element at index 3: " + anArray[3]);
+        System.out.println("Element at index 4: " + anArray[4]);
+        System.out.println("Element at index 5: " + anArray[5]);
+        System.out.println("Element at index 6: " + anArray[6]);
+        System.out.println("Element at index 7: " + anArray[7]);
+        System.out.println("Element at index 8: " + anArray[8]);
+        System.out.println("Element at index 9: " + anArray[9]);
+    }
+
+
+
+
+
+
+
+
+
+}
